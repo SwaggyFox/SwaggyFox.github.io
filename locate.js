67c803b15ebc02a8b624a98e3ed692b385ef41aa
SwaggyFox <1354843637@qq.com>
2017-06-14 18:56:42 +0800

    Create locate.js

--- locate.js
+++ locate.js
@@ -1,15 +1,21 @@
 // JavaScript Document
 // 地图API
+window.onload=function BDmap();
+
 function BDmap(){
 	var map = new BMap.Map("map");
-	var point = new BMap.Point(116.331398,39.897445);
-	map.centerAndZoom(point,12);
-
-	function myFun(result){
-		var cityName = result.name;
-		map.setCenter(cityName);
-		alert("当前定位城市:"+cityName);
-	}
-	var myCity = new BMap.LocalCity();
-	myCity.get(myFun);
+    var point = new BMap.Point(116.331398,39.897445);
+    map.centerAndZoom(point,18);
+    var geolocation = new BMap.Geolocation();
+    geolocation.getCurrentPosition(function(r){
+        if(this.getStatus() == BMAP_STATUS_SUCCESS){
+            var mk = new BMap.Marker(r.point);
+            map.addOverlay(mk);
+            map.panTo(r.point);
+            alert('您的位置：'+r.point.lng+','+r.point.lat);
+        }
+        else {
+            alert('failed'+this.getStatus());
+        }        
+    },{enableHighAccuracy: true})
 }
