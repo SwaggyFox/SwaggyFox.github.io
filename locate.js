2b48b8543432093e48235c5548212ec22cf235c4
SwaggyFox <1354843637@qq.com>
2017-06-14 16:45:58 +0800

    Add files via upload

---
+++ locate.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+// 地图API
+function BDmap(){
+	var map = new BMap.Map("map");
+	var point = new BMap.Point(116.331398,39.897445);
+	map.centerAndZoom(point,12);
+
+	function myFun(result){
+		var cityName = result.name;
+		map.setCenter(cityName);
+		alert("当前定位城市:"+cityName);
+	}
+	var myCity = new BMap.LocalCity();
+	myCity.get(myFun);
+}
